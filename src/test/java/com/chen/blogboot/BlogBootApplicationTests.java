e53a3032b4e48c69a4f6fcbf006b58342180388c
Chen-1026 <1223325118@qq.com>
2021-06-04 09:16:36 +0800

    第一次提交-----搭建开发环境

---
+++ src/test/java/com/chen/blogboot/BlogBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.blogboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
